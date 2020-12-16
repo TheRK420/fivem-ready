405de1a77b6d7e8479ecb656a23c5aed6eb03656
AjayGavde <ajaygavde99@gmail.com>
2020-12-16 15:16:45 +0530

    `Changes in Radio`

--- resources/rp-radio/server.lua
+++ resources/rp-radio/server.lua
@@ -1,20 +1,20 @@
-ESX = nil
+HHCore = nil
+TriggerEvent('hhrp:getSharedObject', function(obj) HHCore = obj end)
 
-TriggerEvent('cc:getSharedObjectterabaap', function(obj) ESX = obj end)
 -- Code
 
-ESX.RegisterUsableItem('radio', function(source, item)
-   local xPlayer = ESX.GetPlayerFromId(source)
+HHCore.RegisterUsableItem('radio', function(source, item)
+   local xPlayer = HHCore.GetPlayerFromId(source)
    TriggerClientEvent('radio:itemradio', source)
 end)
 
 -- checking is player have item
 Citizen.CreateThread(function()
     while true do
-    Citizen.Wait(1000)
-      local xPlayers = ESX.GetPlayers()
+    Citizen.Wait(120000)
+      local xPlayers = HHCore.GetPlayers()
       for i=1, #xPlayers, 1 do
-          local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+          local xPlayer = HHCore.GetPlayerFromId(xPlayers[i])
             if xPlayer ~= nil then
                 if xPlayer.getInventoryItem('radio').count == 0 then
                   local source = xPlayers[i]
