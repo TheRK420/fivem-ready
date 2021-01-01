b49b991213d2d6841a23ba23cb06a72dc92728db
TheLaggyBoi <ajaygavde99@gmail.com>
2021-01-01 14:10:32 +0530

    Merged Few Client Sides

--- resources/[HHRP]/hhrp-binoculars/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-fx_version 'adamant'
-games { 'gta5' }
-
-client_script "binoculars.lua"
-
-server_script "server.lua"
--- resources/[HHRP]/hhrp-blips/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-client_scripts {
-    'client.lua'
-}
-
-fx_version 'adamant'
-games { 'gta5' }
